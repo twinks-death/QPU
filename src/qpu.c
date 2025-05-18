d9948e2d6c6ebed0023e9783f6ceaecac4eb018b
Twink's Death <usedSUV2016@proton.me>
2025-05-18 14:32:18 +0100

    made sdl work on win

--- src/qpu.c
+++ src/qpu.c
@@ -1,27 +1,4 @@
-#include "qpu-lib.h"
-#include "SDL3/SDL.h"
-#include <stdio.h>
-
-int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Window *win = SDL_CreateWindow("SDL3 Test Window",640, 480, SDL_WINDOW_OPENGL);
-    if (!win) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    // Wait for 2 seconds
-    SDL_Delay(2000);
-
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-
-    printf("SDL window opened and closed successfully.\n");
+int main(int argc, char *argv[]) {
     return 0;
 }
 
